d706dc9e1ed2e1afce12a684691e4deac6e44858
xStar <miguelscb0310@gmail.com>
2026-02-25 14:44:47 -0300

    Fixed window initialization, vulkan initialization and made instance creation

--- EmberVox/HelloTriangleApplication.cs
+++ EmberVox/HelloTriangleApplication.cs
@@ -1,11 +1,17 @@
+using Silk.NET.Core;
+using Silk.NET.Core.Native;
+
 namespace EmberVox;
 
-public class HelloTriangleApplication : IDisposable
+public unsafe class HelloTriangleApplication : IDisposable
 {
     private const int WindowWidth = 800;
     private const int WindowHeight = 600;
 
     private IWindow _window;
+
+    private Vk _vk = null!;
+    private Instance _instance;
 
     public void Run()
     {
@@ -20,14 +26,50 @@ public class HelloTriangleApplication : IDisposable
         WindowOptions options = WindowOptions.DefaultVulkan with
         {
             Size = new Vector2D<int>(WindowWidth, WindowHeight),
-            Title = "Vulkan",
+            Title = "EmberVox: Vulkan",
             WindowBorder = WindowBorder.Fixed, // Basically makes it unresizable
         };
 
         _window = Window.Create(options);
+        _window.Initialize();
     }
 
-    private void InitVulkan() { }
+    private void InitVulkan()
+    {
+        _vk = Vk.GetApi();
+        CreateInstance();
+    }
+
+    private void CreateInstance()
+    {
+        ApplicationInfo appInfo = new()
+        {
+            SType = StructureType.ApplicationInfo,
+            PApplicationName = (byte*)SilkMarshal.StringToPtr("EmberVox"),
+            ApplicationVersion = new Version32(1, 0, 0),
+            PEngineName = (byte*)SilkMarshal.StringToPtr("No Engine"),
+            EngineVersion = new Version32(1, 0, 0),
+            ApiVersion = Vk.Version13,
+        };
+
+        byte** extensions = _window.VkSurface!.GetRequiredExtensions(out uint extensionCount);
+
+        InstanceCreateInfo createInfo = new()
+        {
+            SType = StructureType.InstanceCreateInfo,
+            PApplicationInfo = &appInfo,
+            EnabledExtensionCount = extensionCount,
+            PpEnabledExtensionNames = extensions,
+        };
+
+        if (_vk.CreateInstance(&createInfo, null, out _instance) != Result.Success)
+        {
+            throw new Exception("Failed to create Vulkan instance");
+        }
+
+        SilkMarshal.Free((nint)appInfo.PApplicationName);
+        SilkMarshal.Free((nint)appInfo.PEngineName);
+    }
 
     private void MainLoop()
     {
